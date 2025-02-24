3c0b84e990c66fc3f0e822b1304d6987f89025fb
주강 <jkpark@nkia.co.kr>
2025-02-24 20:45:02 +0900

    BundleAnalyzerPlugin 설정 변경

--- mf-host/webpack.config.js
+++ mf-host/webpack.config.js
@@ -58,9 +58,12 @@ module.exports = {
       },
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'disabled',
-      analyzerPort: 8880,
-      generateStatsFile: true,
+      // analyzerMode 옵션을 static 으로 설정하면, 번들링된 결과물을 파일로 저장합니다. (https://www.npmjs.com/package/webpack-bundle-analyzer#static-mode)
+      analyzerMode: 'static',
+      // reportFilename 옵션을 설정하면, 저장된 파일의 이름을 설정할 수 있습니다. (https://www.npmjs.com/package/webpack-bundle-analyzer#reportfilename)
+      reportFilename: 'bundle-report.html',
+      // openAnalyzer 옵션을 true 로 설정하면, 저장된 파일을 자동으로 엽니다. (https://www.npmjs.com/package/webpack-bundle-analyzer#open-analyzer-on-build)
+      openAnalyzer: false,
     }),
   ],
 };
